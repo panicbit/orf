d609165b6305b23e6ad794fc17338317aba45141
rundrop1 <rundrop1@zoho.com>
2016-02-21 16:53:07 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,254 @@
+#![feature(plugin)]
+#![feature(type_macros)]
+#![plugin(phf_macros)]
+extern crate phf;
+
+#[macro_use]
+extern crate nom;
+extern crate memmap;
+extern crate radix_trie;
+
+use radix_trie::Trie;
+use std::str;
+use std::fs::File;
+use std::prelude::*;
+use std::path::Path;
+use nom::{alphanumeric, not_line_ending,line_ending,eof,multispace, GetOutput, newline, eol};
+use nom::IResult;
+use std::iter::Map;
+use std::str::Chars;
+use std::fmt::Display;
+use memmap::{Mmap, Protection};
+use std::iter::IntoIterator;
+use std::vec::*;
+use std::boxed::Box;
+
+#[derive(Debug)]
+pub struct FASTA_AA<'a> {
+    pub id: &'a str,
+    pub sequence: Vec<&'a str>
+}
+
+#[derive(Debug)]
+pub struct FASTA_Nucleotide<'a> {
+    pub id: &'a str,
+    pub sequence: Vec<Nucleotide_Base>
+}
+
+#[derive(Debug, Clone)]
+pub struct AA_Base {
+    pub key: AA_Key
+}
+#[derive(Debug, Clone)]
+pub struct Nucleotide_Base {
+    pub key: FASTA_Nucleotide_Key
+}
+#[derive(Debug,PartialEq,Eq,Clone)]
+#[repr(u8)]
+pub enum FASTA_Nucleotide_Key {
+    A,
+    C,
+    G,
+    T,
+    U,
+    R,
+    Y,
+    K,
+    M,
+    S,
+    W,
+    B,
+    D,
+    H,
+    V,
+    N,
+    Hyphen = b'-',
+    Asterisk = b'*'
+}
+#[derive(Debug, Clone)]
+#[repr(u8)]
+pub enum AA_Key {
+    A, //Alanine
+    B, //Aspartic Acid (D) or Asparagine (N)
+    C, //Cysteine
+    D, //Aspartic Acid
+    E, //Glutamic Acid
+    F, //Phenylalanine
+    G, //Glycine
+    H, //Histidine
+    I, //Isoleucine
+    J, //Leucine (L) or Isoleucine (I)
+    K, //Lysine
+    L, //Leucine
+    M, //Methionine
+    N, //Asparagine
+    O, //Pyrrolysine
+    P, //Proline
+    Q, //Glutamine
+    R, //Arginine
+    S, //Serine
+    T, //Threonine
+    U, //Selenocysteine
+    V, //Valine
+    W, //Tryptophan
+    Y, //Tyrosine
+    Z, //Glutamic Acid (E) or Glutamine (Q)
+    X,
+    Asterisk = b'*',
+    Hyphen = b'-',
+    Error
+}
+/*
+const Amino_Acid = {
+    //Alanine
+    'A' => "GCA",
+    'A' => "GCG",
+    'A' => "GCC",
+    'A' => "GCT",
+    //Cysteine
+    'C' => "TGT",
+    'C' => "TGC",
+    //Apsartic Acid
+    'D' => "GAC",
+    'D' => "GAT",
+    //Glutamic Acid
+    'E' => "GAA",
+    'E' => "GAG",
+    //Phenylalanine
+    'F' => "TTT",
+    'F' => "TTC",
+    'G' => "GGA",
+    'G' => "GGG",
+    'G' => "GGC",
+    'G' => "GGT",
+    //Histidine
+    'H' => "CAC",
+    'H' => "CAT",
+    //Isoleucine
+    'I' => "ATT",
+    'I' => "ATC",
+    'I' => "ATA",
+    //Leucine
+    'L' => "TTG",
+    'L' => "TTA",
+    'L' => "CTA",
+    'L' => "CTC",
+    'L' => "CTG",
+    'L' => "CTT",
+    //Lysine
+    'K' => "AAA",
+    'K' => "AAG",
+    //Methionine
+    'M' => "ATG",
+    //Asparagine
+    'N' => "AAT",
+    'N' => "AAC",
+    //Pyrrolysine
+    'O' => "UAG",
+    //Proline
+    'P' => "CCA",
+    'P' => "CCG",
+    'P' => "CCC",
+    'P' => "CCT",
+    //Glutamine
+    'Q' => "CAA",
+    'Q' => "CAG",
+    //Arginine
+    'R' => "AGA",
+    'R' => "AGG",
+    //Serine
+    'S' => "AGT",
+    'S' => "AGC",
+    //Threonine
+    'T' => "ACA",
+    'T' => "ACG",
+    'T' => "ACC",
+    'T' => "ACT",
+    //Selenocysteine
+    'U' => "UGA",
+    //Valine
+    'V' => "GTA",
+    'V' => "GTG",
+    'V' => "GTC",
+    'V' => "GTT",
+    //Tryptophan
+    'W' => "TGG",
+    'Y' => "TAT",
+    'Y' => "TAC"
+};
+*/
+static CODONS: phf::Map<&'static str, &'static str> = phf_map! {
+    //Alanine
+    "GCA" => "A",
+    "GCG" => "A",
+    "GCC" => "A",
+    "GCT" => "A",
+    //Aspartic_Acid (D)
+    //Asparagine (N)
+    //Cysteine
+    "TGT" => "C",
+    "TGC" => "C",
+    //Aspartic_Acid
+    "GAC" => "D",
+    "GAT" => "D",
+    //Glutamic_Acid
+    "GAA" => "E",
+    "GAG" => "E",
+    //Phenylalanine
+    "TTT" => "F",
+    "TTC" => "F",
+    //Glycine
+    "GGA" => "G",
+    "GGG" => "G",
+    "GGC" => "G",
+    "GGT" => "G",
+    //Histidine
+    "CAC" => "H",
+    "CAT" => "H",
+    //Isoleucine
+    "ATT" => "I",
+    "ATC" => "I",
+    "ATA" => "I",
+    //Leucine (L)
+    "TTG" => "L",
+    "TTA" => "L",
+    "CTA" => "L",
+    "CTC" => "L",
+    "CTG" => "L",
+    "CTT" => "L",
+    //Lysine (K)
+    "AAA" => "K",
+    "AAG" => "K",
+    //Methionine (M)
+    "ATG" => "M",
+    //Asparagine (N)
+    "AAT" => "N",
+    "AAC" => "N",
+    //Pyrrolysine (O) Special Stop Codon
+    "UAG" => "O",
+    //Proline (P)
+    "CCA" => "P",
+    "CCG" => "P",
+    "CCC" => "P",
+    "CCT" => "P",
+    //Glutamine (Q)
+    "CAA" => "Q",
+    "CAG" => "Q",
+    //Arginine (R)
+    "AGA" => "R",
+    "AGG" => "R",
+    "CGT" => "R",
+    "CGC" => "R",
+    "CGA" => "R",
+    "CGG" => "R",
+    //Serine (S)
+    "AGT" => "S",
+    "AGC" => "S",
+    "TCT" => "S",
+    "TCC" => "S",
+    "TCA" => "S",
+    "TCG" => "S",
+    //Threonine (T)
     "ACA" => "T",
     "ACG" => "T",
     "ACC" => "T",
