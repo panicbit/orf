e149b377a640e2121a3aa15b58f18c0350d79c60
panicbit <panicbit.dev@gmail.com>
2016-04-19 11:34:07 +0200

    Use number of cpus as number of threads by default

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate transcriptome_translation;
 extern crate memmap;
+extern crate num_cpus;
 
 use std::fs::File;
 use memmap::{Mmap, Protection};
@@ -8,7 +9,7 @@ fn main() {
     let mmap = Mmap::open_path("test/test-nucleo.FASTA", Protection::Read).unwrap();
     let input = unsafe { mmap.as_slice() };
     let mut output_file = File::create("results.txt").unwrap();
-    let n_threads = 4;
+    let n_threads = num_cpus::get() as u32;
 
     transcriptome_translation::start_parse(input, &mut output_file, n_threads);
 
